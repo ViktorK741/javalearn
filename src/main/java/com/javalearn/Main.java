835cc9e724d7c2f7a8092957afde480c87535d48
kvs777 <lg190701kvs@gmail.com>
2022-02-05 14:43:32 +0200

    make branch  src/main/java/com/io/ Project_new1/

--- src/main/java/com/javalearn/Main.java
+++ src/main/java/com/javalearn/Main.java
@@ -1,4 +1,28 @@
 package com.javalearn;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
+    public static void main(String[] args) {
+        //Path testFilePath = (Path) Paths.get("C:\\java_projects\\tmp\\test.txt");
+
+        Path testFilePath = Paths.get("C:\\java_projects\\tmp\\test.txt");
+
+        Path fileName = testFilePath.getFileName();
+        System.out.println(fileName);
+
+        Path parent = testFilePath.getParent();
+        System.out.println(parent);
+
+        Path root = testFilePath.getRoot();
+        System.out.println(root);
+
+        boolean endWithTxt = testFilePath.endsWith("Desktop\\testFile.txt");
+        System.out.println(endWithTxt);
+
+        boolean startsWithLalala = testFilePath.startsWith("lalalala");
+        System.out.println(startsWithLalala);
+    }
+
 }
--- src/main/java/com/javalearn/test1/test1_io.java
+++ src/main/java/com/javalearn/test1/test1_io.java
@@ -1,6 +1,27 @@
 package com.javalearn.test1;
 
+import java.nio.file.Files;
+import java.nio.file.*;
+
 public class test1_io {
+    public static void main(String[] args) {
+          //Path testFilePath = (Path) Paths.get("C:\\java_projects\\tmp\\test.txt");
+
+        Path testFilePath = Paths.get("C:\\java_projects\\tmp\\test.txt");
+
+        Path fileName = testFilePath.getFileName();
+        System.out.println(fileName);
+
+        Path parent = testFilePath.getParent();
+        System.out.println(parent);
+
+        Path root = testFilePath.getRoot();
+        System.out.println(root);
+
+        boolean endWithTxt = testFilePath.endsWith("Desktop\\testFile.txt");
+        System.out.println(endWithTxt);
+
+        boolean startsWithLalala = testFilePath.startsWith("lalalala");
+        System.out.println(startsWithLalala);
+    }
 }
-
-
